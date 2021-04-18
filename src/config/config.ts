f8bede3d1bf6d49ee85eee02f0c1429fd30822aa
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-04-19 02:26:30 +0700

    Added APP_NAME in config

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,14 +4,14 @@ dotenv.config();
 
 const SERVER_PORT = process.env.SERVER_PORT || 57898;
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
+const APP_NAME = process.env.APP_NAME || 'DJIM Paspor Online API';
 
 const SERVER = {
   hostname: SERVER_HOSTNAME,
-  port: SERVER_PORT
+  port: SERVER_PORT,
+  app: APP_NAME
 }
 
-const config = {
+export default {
   server: SERVER
 };
-
-export default config;

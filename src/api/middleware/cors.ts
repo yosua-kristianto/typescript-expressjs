6c2a16da3708ad08ffd9e4535ef21393cbcfb513
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-04-19 02:27:06 +0700

    Added cors mioddleware

--- src/api/middleware/cors.ts
+++ src/api/middleware/cors.ts
@@ -1,16 +1,25 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 const middleware = express();
 
-middleware.use((req: Request, res: Response, next: NextFunction) => {
+/**
+ * cors.ts
+ *  This middleware contain 
+ */
+
+export default (req: Request, res: Response, next: NextFunction) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers', 
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
 
+  console.log('Middleware Running');
+
   if(req.method == 'OPTIONS'){
     res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
     return res.status(200).json({});
   }
-});
+
+  next();
+};

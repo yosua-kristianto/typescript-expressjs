859ef88d9436c1ce159d0c1bc1665ae79fe7f649
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-04-20 17:39:56 +0700

    Tiding up config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,46 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const SERVER_PORT = process.env.SERVER_PORT || 57898;
-const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const APP_NAME = process.env.APP_NAME || 'DJIM Paspor Online API';
-
-const SERVER = {
-  hostname: SERVER_HOSTNAME,
-  port: SERVER_PORT,
-  app: APP_NAME
-}
+import { Dialect } from 'sequelize';
 
 export default {
-  server: SERVER
-};
+  /*
+  |--------------------------------------------------------------------------
+  | Server Identification
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you register the server identification. 
+  | This value is used when the framework needs to place application's name.
+  | 
+  */
+  server: {
+    hostname  : process.env.SERVER_HOSTNAME || 'localhost',
+    port      : process.env.SERVER_PORT || 57898,
+    app       : process.env.APP_NAME || 'DJIM Paspor Online API',
+    env       : process.env.APP_ENV || 'production'
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Default Database Connection Configuration
+  |--------------------------------------------------------------------------
+  |
+  | Here you may specify which of the database connections below you wish
+  | to use as your default connection for all database work. Of course
+  | you may use many connections at once using the Database library.
+  |
+  */
+  database: {
+
+    main: {
+      dialect   : (process.env.DB_CONNECTION_DIALECT || "postgres") as Dialect,
+      uri       : process.env.DB_HOSTNAME || "127.0.0.1",
+      port      : process.env.DB_PORT || "5432",
+      database  : process.env.DB_DATABASE || '',
+      username  : process.env.DB_USERNAME || 'root',
+      password  : process.env.DB_PASSWORD || ''
+    }
+
+  }
+  
+}

7fe9a720ee0a8a1c3c2d1ffb562f9d45f5c0d6d2
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-04-20 17:32:50 +0700

    Deleting dist folder

--- dist/api/controller/controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Controller = void 0;
-class Controller {
-    doSomething() {
-    }
-}
-exports.Controller = Controller;

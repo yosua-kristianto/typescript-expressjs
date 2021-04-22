4ac3d957d3d7215fcf59b1007c77325a84a5beee
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-04-22 19:23:36 +0700

    Added exception domain, and  handled exception response

--- src/config/exception.ts
+++ src/config/exception.ts
@@ -1,6 +1,42 @@
 import process from "process";
 import { Log } from "./logging";
 
+import { Response } from 'express';
+import { BaseResponse } from '../model/dto/base-response';
+
+/**
+ * ErrorHandler
+ *  An object to handle Exceptions that 
+ *  shown on Api Response.
+ */
+export class ErrorHandler extends Error {
+
+  statusCode: number;
+  message: string;
+
+  constructor(statusCode: number, message: string) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+
+}
+
+/**
+ * This is where error being returned as response.
+ */
+export const handleError = (res: Response, err: any): void => {
+  Log.e("Unhandled Exception", err);
+  const { statusCode, message } = err;
+
+  new BaseResponse()
+        .error(
+          message,
+          res,
+          statusCode
+  );
+}
+
 /**
  * exception.ts
  *  This file contains the uncaught exception reporting

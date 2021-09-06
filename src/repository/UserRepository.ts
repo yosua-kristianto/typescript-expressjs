5f0558d25440629ebd3e8f1793c04a0dae8296c1
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-09-06 12:21:29 +0700

    Update UserRepository so it's implement Arrow Function more

---
+++ src/repository/UserRepository.ts
@@ -0,0 +1,33 @@
+import User from '../model/entity/user';
+import { CreateUserDTO } from '../model/dto/request/CreateUserDTO';
+
+export interface UserRepository {
+  
+  /**
+   * findUserById
+   *  A repository to find user data by id.
+   * 
+   * @param number id
+   * 
+   * @return /model/entity/User
+   */
+  findUserById(id: number): Promise<User>;
+
+  /**
+   * getAllUser
+   *  A repository to get all user data.
+   * 
+   * @return Array<User>
+   */
+  getAllUser(): Promise<User[]>;
+
+  /**
+   * createUser
+   *  A repository to create new User by
+   *  designed DTO.
+   * 
+   * @return User
+   */
+  createUser(request: CreateUserDTO): Promise<User>;
+
+}
---
+++ src/repository/impl/UserRepositoryImpl.ts
@@ -0,0 +1,28 @@
+import { UserNotFoundException } from '../../common/exception/UserNotFoundException';
+import { CreateUserDTO } from '../../model/dto/request/CreateUserDTO';
+import User from '../../model/entity/user';
+import { UserRepository } from '../UserRepository';
+
+class UserRepositoryImpl implements UserRepository {
+
+  findUserById = async(id: number): Promise<User> =>
+    User.findOne({
+      where: {
+        is_deleted: 0,
+        id: id
+      }
+    }).then(resultSet => {
+      if(resultSet === null) throw new UserNotFoundException();
+
+      return resultSet;
+    });
+
+  getAllUser = async(): Promise<Array<User>> => User.findAll({where: {is_deleted: 0}});
+
+  createUser(request: CreateUserDTO): Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+    
+}
+
+export default new UserRepositoryImpl();

e39f4c927b3f12013d1f0ca9d0cc07a9c9f8378e
Yosua Kristianto <yosuakristianto754@gmail.com>
2022-07-15 20:46:46 +0700

    Added space

--- src/main/handler/ExampleControllerHandler.ts
+++ src/main/handler/ExampleControllerHandler.ts
@@ -5,7 +5,7 @@ export class ExampleControllerHandler {
   public addition = (firstParam: number, secondParam: number): number => firstParam + secondParam;
 
   public ageValidation = (age: number): true => {
-    if(age < 18) {
+    if (age < 18) {
       throw new ExampleAgeBelowEightTeenException();
     }
 

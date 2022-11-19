139970843f8b4bf032672bfb5adba764d837abb2
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2022-11-20 01:58:44 +0700

    Tiding up test files and added jest implementation

---
+++ src/test/Index.test.ts
@@ -0,0 +1,20 @@
+/**
+ * WRITING TEST
+ *
+ * You can specify every unit Tests in here.
+ */
+import {ExampleControllerHandler} from "../main/handler/ExampleControllerHandler";
+import {ExampleAgeBelowEightTeenException} from "../main/common/exception/example/ExampleAgeBelowEightTeenException";
+
+test("[POSITIVE] Should be return true when given 19", () => {
+  expect(new ExampleControllerHandler().ageValidation(19))
+    .toBe(true);
+});
+
+test("[NEGATIVE] Should be throw age validation error when given under 18", () => {
+  expect(() => {
+    new ExampleControllerHandler().ageValidation(13)
+  })
+    .toThrow(new ExampleAgeBelowEightTeenException().message);
+});
+

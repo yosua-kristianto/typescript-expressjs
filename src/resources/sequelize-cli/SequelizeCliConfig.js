a25f66ab1ab11625186e00c6cee7b4c9ac780a77
Yosua Kristianto <yosua_kristianto144@outlook.com>
2025-06-05 00:18:09 +0700

    Fixing configurations

--- src/resources/sequelize-cli/SequelizeCliConfig.js
+++ src/resources/sequelize-cli/SequelizeCliConfig.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 module.exports = {
   "main": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOSTNAME,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_CONNECTION
+    "username": process.env.DB_MAIN_USERNAME,
+    "password": process.env.DB_MAIN_PASSWORD,
+    "database": process.env.DB_MAIN_DATABASE,
+    "host": process.env.DB_MAIN_HOSTNAME,
+    "port": process.env.DB_MAIN_PORT,
+    "dialect": process.env.DB_MAIN_CONNECTION
   },
 }
